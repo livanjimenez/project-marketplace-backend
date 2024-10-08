940f758ce60495b5345d78b047fdc0352d2afe7d
livanjimenez <jimenez.livan14@gmail.com>
2024-10-08 14:37:19 -0400

    p

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,4 +4,19 @@ import { UsersService } from '../users/users.service';
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  async signIn(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+
+    const { password, ...result } = user;
+
+    // TODO: Implement JWT token generation
+    // instead of returning the user object
+
+    return result;
+  }
 }
