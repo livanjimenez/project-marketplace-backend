06128c1b91bdf5accb3fd1c5e7e60fdbfd84f308
livanjimenez <jimenez.livan14@gmail.com>
2024-10-08 19:38:51 -0400

    p

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,20 +7,21 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      entities: [],
-      synchronize: true,
-    }),
-    AuthModule,
-    UsersModule,
-  ],
+  // imports: [
+  //   TypeOrmModule.forRoot({
+  //     type: 'postgres',
+  //     host: 'localhost',
+  //     port: 3306,
+  //     username: 'root',
+  //     password: 'root',
+  //     database: 'test',
+  //     entities: [],
+  //     synchronize: true,
+  //   }),
+  //   AuthModule,
+  //   UsersModule,
+  // ],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,13 @@
+import { CanActivate, Injectable, ExecutionContext } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    return true;
+  }
+}
