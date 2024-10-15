d20058c80236f59eee7827dbafa5296ca3116efb
livanjimenez <jimenez.livan14@gmail.com>
2024-10-15 16:14:04 -0400

    small change

--- src/listings/entities/listing.entity.ts
+++ src/listings/entities/listing.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity({ name: 'listings' })
+@Entity({ name: 'listing' })
 export class Listing {
   @PrimaryGeneratedColumn()
   id: number;
